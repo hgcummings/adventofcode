f8b1ae1010113ba6ae60571d095ab5469ec939ad
Harry Cummings <harry@xlr3.com>
2021-12-08 19:30:33 +0000

    2015 day 2

---
+++ 2015/day02/part2.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const readline = require('readline');
+const _ = require('lodash');
+
+async function processLineByLine() {
+    const fileStream = fs.createReadStream(process.argv[2] || 'input.txt');
+  
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity
+    });
+  
+    let total = 0;
+    for await (const line of rl) {
+        const dims = line.split("x").map(d => parseInt(d, 10)).sort((a, b) => a - b);
+        total += (2 * (dims[0] + dims[1])) + (dims[0] * dims[1] * dims[2]);
+    }
+
+    console.log(total);
+  }
+  
+  processLineByLine();
