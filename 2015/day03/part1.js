a5c22c25f14c4949f50e5f44843ca37b2f970ddf
Harry Cummings <harry@xlr3.com>
2021-12-08 19:30:42 +0000

    2015 day 3

---
+++ 2015/day03/part1.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const readline = require('readline');
+const _ = require('lodash');
+
+async function processLineByLine() {
+    const fileStream = fs.createReadStream(process.argv[2] || 'input.txt');
+  
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity
+    });
+  
+    let instructions;
+    for await (const line of rl) {
+        instructions = line.split("");
+        break;
+    }
+
+    let houses = [];
+    let pos = [instructions.length, instructions.length];
+
+    houses[pos[0]] = [];
+    houses[pos[0]][pos[1]] = 1;
+
+    for (let inst of instructions) {
+      switch (inst) {
+        case ">":
+          pos[0] += 1;
+          break;
+        case "<":
+          pos[0] -= 1;
+          break;
+        case "v":
+          pos[1] += 1;
+          break;
+        case "^":
+          pos[1] -= 1;
+          break;
+        default:
+          break;
+      }
+
+      houses[pos[0]] = houses[pos[0]] || [];
+      houses[pos[0]][pos[1]] = (houses[pos[0]][pos[1]] || 0) + 1;
+    }
+
+    let count = 0;
+    for (let row of houses) {
+      if (row) {
+        for (let house of row) { 
+          if (house) {
+            count += 1;
+          }
+        }
+      }
+    }
+
+    console.log(count);
+  }
+  
+  processLineByLine();
