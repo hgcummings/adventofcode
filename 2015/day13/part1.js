138c65460b921cf249271a92ffe31079d3feb783
Harry Cummings <harry@xlr3.com>
2021-12-16 14:33:09 +0000

    2015 day 13

---
+++ 2015/day13/part1.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const readline = require('readline');
+const _ = require('lodash');
+const { permutations } = require('combinatorial-generators');
+
+async function processLineByLine() {
+    const fileStream = fs.createReadStream(process.argv[2] || 'input.txt');
+    
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+    
+    const people = [];
+    const rels = {};
+
+    const expr = /(?<a>[A-Za-z]+) would (?<dir>(gain)|(lose)) (?<mag>[0-9]+) happiness units by sitting next to (?<b>[A-Za-z]+)./;
+    for await (const line of rl) {
+        const {a, b, dir, mag} = expr.exec(line).groups;
+
+        if (!people.includes(a)) {
+            people.push(a);
+            rels[a] = {};
+        }
+
+        rels[a][b] = mag * (dir === "gain" ? 1 : -1);
+    }
+
+    let maxScore = -Infinity;
+
+    for (let p of permutations(people)) {
+        let score = 0;
+        for (let i = 0; i < p.length; ++i) {
+            let left = p[i];
+            let right = p[(i + 1) % p.length];
+            score += rels[left][right] + rels[right][left];
+        }
+        if (score > maxScore) {
+            maxScore = score;
+        }
+    }
+
+    console.log(maxScore);
+}
+
+processLineByLine();
