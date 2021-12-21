b2ec41ee2d540abacf88aefa3ad411b54725362e
Harry Cummings <harry@xlr3.com>
2021-12-21 10:13:37 +0000

    Add performance metric to starter script

--- starter/part1.js
+++ starter/part1.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const readline = require('readline');
 const _ = require('lodash');
+const {performance} = require('perf_hooks');
 
+const startTime = performance.now();
 async function processLineByLine() {
     const fileStream = fs.createReadStream(process.argv[2] || 'input.txt');
     
@@ -14,8 +16,10 @@ async function processLineByLine() {
     for await (const line of rl) {
         lines.push(line);
     }
-    
+
     console.log(lines.length);
+
+    console.log(`(Took ${Math.round(performance.now() - startTime) / 1000}s)`);
 }
 
 processLineByLine();
