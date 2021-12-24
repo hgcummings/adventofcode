37f1fbdb9598a51cec92c435cfd281a3beda513a
Harry Cummings <harry@xlr3.com>
2021-12-24 12:44:59 +0000

    2021 day 24 part 1 new approach
    Based on suggestion from https://www.reddit.com/r/adventofcode/comments/rnj7r7/2021_day_24_how_do_you_approach_this/hpsojxn

--- 2021/day24/part1.js
+++ 2021/day24/part1.js
@@ -2,99 +2,78 @@ const fs = require('fs');
 const readline = require('readline');
 const _ = require('lodash');
 const {performance} = require('perf_hooks');
+const ArrayKeyedMap = require('array-keyed-map')
 
 const startTime = performance.now();
-async function processLineByLine() {
-    const fileStream = fs.createReadStream(process.argv[2] || 'input.txt');
-    
-    const rl = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity
-    });
 
-    const expr = /(?<op>[a-z]+) (?<a>\-?[0-9w-z]+) ?(?<b>\-?[0-9w-z]+)?/
+const constants = [
+    [1,14,12],
+    [1,15,7],
+    [1,12,1],
+    [1,11,2],
+    [26,-5,4],
+    [1,14,15],
+    [1,15,11],
+    [26,-13,5],
+    [26,-16,3],
+    [26,-8,9],
+    [1,15,2],
+    [26,-8,3],
+    [26,0,3],
+    [26,-4,11]
+];
 
-    let instructions = [];
-    for await (const line of rl) {
-        const {op, a, b} = expr.exec(line).groups;
-        instructions.push({
-            op,
-            a: isFinite(parseInt(a, 10)) ? parseInt(a, 10) : a,
-            b: isFinite(parseInt(b, 10)) ? parseInt(b, 10) : b
-        });
-    }
+let states = new ArrayKeyedMap();
+states.set([0,0,0],0);
 
-    const constants = [
-        [1,14,12],
-        [1,15,7],
-        [1,12,1],
-        [1,11,2],
-        [26,-5,4],
-        [1,14,15],
-        [1,15,11],
-        [26,-13,5],
-        [26,-16,3],
-        [26,-8,9],
-        [1,15,2],
-        [26,-8,3],
-        [26,0,3],
-        [26,-4,11]
-    ];
+const test = [9,9,9,4,1,9,1,1,1,9,1,1,4,9];
 
-    function findValid(prevZ, c, prefix) {
-        if (c < 6) {
-            console.log(prefix);
-            console.log(`(${Math.round(performance.now() - startTime) / 1000}s elapsed)`);
-        }
-
-        /*  The following code should be equivalent to...
+for (let i = 0; i < constants.length; ++i) {
+    const newStates = new ArrayKeyedMap();
+    let duplicates = 0;
+    for (const [state, val] of states.entries()) {
+        // for (let d = test[i]; d === test[i]; --d) {
+        for (let d = 9; d > 0; --d) {
+            let [x, y, z] = state;
+            let w = d;
+            // console.log("--------");
+            // console.log("a", w, x, y, z);
             x = z;
+            if (x < 0) {
+                continue;
+            }
             x = x % 26;
-            z = Math.floor(z / ${c[0]});
-            x = x + ${c[1]};
+            z = Math.trunc(z / constants[i][0]);
+            // console.log("b", w, x, y, z);
+            x = x + constants[i][1];
             x = x === w ? 0 : 1;
             y = (25 * x) + 1;
             z = z * y;
-            y = (w + ${c[2]}) * x;
+            // console.log("c", w, x, y, z);
+            y = (w + constants[i][2]) * x;
             z = z + y;
-            if (z < 0) { return false; }
-        */
-
-        let initX = prevZ % 26;
-        // z is positive due to the check before the recursive call,
-        // and constants[c] is only ever positive (1 or 26), so we
-        // can use ~~ as a more efficient Math.trunc here. 
-        let initZ = ~~(prevZ/constants[c][0]);
-        initX = initX + constants[c][1];
-
-        for (let w = 9; w > 0; --w) {
-            let x = initX === w ? 0 : 1;
-            let y = (25 * x) + 1;
-            let z = initZ * y;
-            y = (w + constants[c][2]) * x;
-            z = z + y;
-            if (c === 13) {
-                if (z === 0) {
-                    return prefix + w;
+            // console.log("d", w, x, y, z);
+            if (i < 13 || z === 0) {
+                if (newStates.has([x,y,z])) {
+                    duplicates += 1;
                 }
-            } else {
-                if (z < 0) {
-                    continue;
-                }
-                let next = findValid(z, c + 1, prefix + (w * Math.pow(10, 13 - c)));
-                if (next) {
-                    return next;
-                } else {
-                    continue;
+                if (!newStates.has([x,y,z])) {
+                    newStates.set([x,y,z], val + (d * (Math.pow(10, 13 - i))));
                 }
             }
         }
-        return false;
     }
-
-    console.log(findValid(0,0,0));
-
-    console.log(`(Took ${Math.round(performance.now() - startTime) / 1000}s)`);
+    states = newStates;
+    console.log("------");
+    console.log(duplicates);
+    console.log(states.size);
 }
 
-processLineByLine();
+let maxVal = -Infinity;
+for (const val of states.values()) {
+    if (val > maxVal) {
+        maxVal = val;
+    }
+}
+console.log(maxVal);
+console.log(`(Took ${Math.round(performance.now() - startTime) / 1000}s)`);
