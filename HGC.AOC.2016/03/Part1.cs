dcc08fa7d5a010159f73f38335d0469d32f8c3bb
Harry Cummings <harry@xlr3.com>
2022-10-23 15:12:19 +0100

    2016 day 3

---
+++ HGC.AOC.2016/03/Part1.cs
@@ -0,0 +1,26 @@
+﻿using HGC.AOC.Common;
+
+namespace HGC.AOC._2016._03;
+
+public class Part1 : ISolution
+{
+    public string? Answer()
+    {
+        var input = this.ReadInputLines("input.txt");
+        var possibleCount = 0;
+
+        foreach (var line in input)
+        {
+            var sides = line.Trim()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(Int32.Parse).OrderByDescending(s => s).ToList();
+
+            if (sides[0] < sides.Skip(1).Sum())
+            {
+                possibleCount++;
+            }
+        }
+
+        return possibleCount.ToString();
+    }
+}
---
+++ HGC.AOC.2016/03/Part2.cs
@@ -0,0 +1,42 @@
+﻿using HGC.AOC.Common;
+
+namespace HGC.AOC._2016._03;
+
+public class Part2 : ISolution
+{
+    public string? Answer()
+    {
+        var input = this.ReadInputLines("input.txt");
+        var possibleCount = 0;
+        var buffer = new List<List<int>>();
+
+        foreach (var line in input)
+        {
+            buffer.Add(line.Trim()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(Int32.Parse).ToList());
+
+            if (buffer.Count == 3)
+            {
+                for (var i = 0; i < 3; ++i)
+                {
+                    var sides = new List<int>
+                    {
+                        buffer[0][i], buffer[1][i], buffer[2][i]
+                    };
+                    
+                    sides.Sort();
+                    
+                    if (sides[2] < sides[0] + sides[1])
+                    {
+                        possibleCount++;
+                    }
+                }
+                
+                buffer.Clear();
+            }
+        }
+
+        return possibleCount.ToString();
+    }
+}
