90d85f70f6d39a51ba7ffde013de76c368ee129a
Harry Cummings <harry@xlr3.com>
2022-10-23 17:31:32 +0100

    Include timings in C# runner output

---
+++ HGC.AOC.Common/StringHelpers.cs
@@ -0,0 +1,19 @@
+﻿namespace HGC.AOC.Common;
+
+public static class StringHelpers
+{
+    public static string ExpandEdges(this string input, int length)
+    {
+        if (input.Length >= length)
+        {
+            return input;
+        }
+        else
+        {
+            var padding = length - input.Length;
+            return input
+                .PadLeft(input.Length + padding / 2, input[0])
+                .PadRight(length, input[^1]);
+        }
+    }
+}
--- HGC.AOC.Runner/Program.cs
+++ HGC.AOC.Runner/Program.cs
@@ -1,4 +1,14 @@
-﻿var answer = HGC.AOC._2016.Entry.Current().Answer();
+﻿using System.Diagnostics;
+using HGC.AOC.Common;
+
+var solution = HGC.AOC._2016.Entry.Current();
+
+Console.WriteLine($"= Running {solution.GetType().FullName} =".ExpandEdges(80));
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+var answer = solution.Answer();
+stopwatch.Stop();
+Console.WriteLine($"= Completed in {stopwatch.ElapsedMilliseconds}ms =".ExpandEdges(80));
 Console.WriteLine(answer);
 if (answer != null)
 {
