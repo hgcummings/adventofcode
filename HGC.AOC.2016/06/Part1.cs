0b4258a3adfe8ba5a3e52f1dde0015fc8aa43e86
Harry Cummings <harry@xlr3.com>
2022-10-23 17:31:13 +0100

    2016 day 6

---
+++ HGC.AOC.2016/06/Part1.cs
@@ -0,0 +1,33 @@
+﻿using HGC.AOC.Common;
+
+namespace HGC.AOC._2016._06;
+
+public class Part1 : ISolution
+{
+    public string? Answer()
+    {
+        var input = this.ReadInputLines("input.txt");
+
+        var counts = new List<Tally<char>>();
+        
+        foreach (var rawLine in input)
+        {
+            var line = rawLine.Trim();
+
+            if (counts.Count == 0)
+            {
+                for (var i = 0; i < line.Length; ++i)
+                {
+                    counts.Add(new Tally<char>());
+                }
+            }
+            
+            for (var i = 0; i < line.Length; ++i)
+            {
+                counts[i].Increment(line[i]);
+            }
+        }
+
+        return String.Join("", counts.Select(d => d.Highest));
+    }
+}
---
+++ HGC.AOC.Common/Tally.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Concurrent;
+
+namespace HGC.AOC.Common;
+
+public class Tally<TKey> : IEnumerable<KeyValuePair<TKey, int>> where TKey : notnull
+{
+    private readonly ConcurrentDictionary<TKey, int> _table; 
+
+    public Tally()
+    {
+        _table = new ConcurrentDictionary<TKey, int>();
+    }
+
+    public TKey Highest => _table.MaxBy(entry => entry.Value).Key;
+    public TKey Lowest => _table.MinBy(entry => entry.Value).Key;
+
+    public void Increment(TKey key)
+    {
+        _table.AddOrUpdate(key, _ => 0, (_, x) => x + 1);
+    }
+
+    public IEnumerator<KeyValuePair<TKey, int>> GetEnumerator()
+    {
+        return _table.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
