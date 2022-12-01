832c725e26b3a678661887c9b89d99812eb078ca
Harry Cummings <harry@xlr3.com>
2022-12-01 05:13:00 +0000

    2016 day 11 WIP

---
+++ HGC.AOC.2016/11/Part1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Text.RegularExpressions;
+using HGC.AOC.Common;
+
+namespace HGC.AOC._2016._11;
+
+public struct State
+{
+    private sealed class StateEqualityComparer : IEqualityComparer<State>
+    {
+        public bool Equals(State x, State y)
+        {
+            return x.GeneratorPositions.SequenceEqual(y.GeneratorPositions) &&
+                   x.MicrochipPositions.SequenceEqual(x.MicrochipPositions);
+        }
+
+        public int GetHashCode(State obj)
+        {
+            return HashCode.Combine(obj.GeneratorPositions[0], obj.MicrochipPositions[0]);
+        }
+    }
+
+    public static IEqualityComparer<State> StateComparer { get; } = new StateEqualityComparer();
+
+    public int[] GeneratorPositions;
+    public int[] MicrochipPositions;
+
+    public State(int[] generatorPositions, int[] microchipPositions)
+    {
+        GeneratorPositions = generatorPositions;
+        MicrochipPositions = microchipPositions;
+    }
+}
+
+public class Part1 : ISolution
+{
+    public string? Answer()
+    {
+        return null;
+    }
+}
