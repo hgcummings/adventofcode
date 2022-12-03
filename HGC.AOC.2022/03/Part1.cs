4ff8463a92f90207d28750d7941f4e2733e12c1b
Harry Cummings <harry@xlr3.com>
2022-12-03 08:18:31 +0000

    2022 day 3

---
+++ HGC.AOC.2022/03/Part1.cs
@@ -0,0 +1,23 @@
+﻿using HGC.AOC.Common;
+
+namespace HGC.AOC._2022._03;
+
+public class Part1 : ISolution
+{
+    public object? Answer()
+    {
+        var input = this.ReadInputLines("input.txt");
+        var total = 0;
+        foreach (var line in input)
+        {
+            var pocket1 = line.Substring(0, line.Length / 2);
+            var pocket2 = line.Substring(line.Length / 2);
+
+            total += Priority(pocket1.Intersect(pocket2).Single());
+        }
+
+        return total;
+    }
+
+    private int Priority(char item) => item > 96 ? item - 96 : item - 38;
+}
---
+++ HGC.AOC.2022/03/Part2.cs
@@ -0,0 +1,16 @@
+﻿using HGC.AOC.Common;
+
+namespace HGC.AOC._2022._03;
+
+public class Part2 : ISolution
+{
+    public object? Answer()
+    {
+        return this.ReadInputLines("input.txt")
+            .Chunk(3)
+            .Select(group => group[0].Intersect(group[1]).Intersect(group[2]).Single())
+            .Sum(Priority);
+    }
+
+    private int Priority(char item) => item > 96 ? item - 96 : item - 38;
+}
