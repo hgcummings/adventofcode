f503a0c01230987cd1576be69452766a43e8edca
Harry Cummings <harry@xlr3.com>
2022-12-04 05:15:00 +0000

    2022 day 4

---
+++ HGC.AOC.2022/04/Part1.cs
@@ -0,0 +1,44 @@
+﻿using HGC.AOC.Common;
+
+namespace HGC.AOC._2022._04;
+
+public class Part1 : ISolution
+{
+    public object? Answer()
+    {
+        var input = this.ReadInputLines("input.txt");
+        var total = 0;
+        foreach (var line in input)
+        {
+            var ranges = line.Split(",").Select(def => ParseRange(def)).ToList();
+            if (ranges[0].Contains(ranges[1]) || ranges[1].Contains(ranges[0]))
+            {
+                total++;
+            }
+        }
+        return total;
+    }
+
+    private AssignedRange ParseRange(string def)
+    {
+        var indices = def.Split("-").Select(Int32.Parse).ToList();
+        return new AssignedRange(indices[0], indices[1]);
+    }
+
+    struct AssignedRange
+    {
+        public int Start { get; }
+        public int End { get; }
+
+        public AssignedRange(int start, int end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public bool Contains(AssignedRange other)
+        {
+            return this.Start <= other.Start && this.End >= other.End;
+        }
+    }
+}
---
+++ HGC.AOC.2022/04/Part2.cs
@@ -0,0 +1,46 @@
+﻿using HGC.AOC.Common;
+
+namespace HGC.AOC._2022._04;
+
+public class Part2 : ISolution
+{
+    public object? Answer()
+    {
+        var input = this.ReadInputLines("input.txt");
+        var total = 0;
+        foreach (var line in input)
+        {
+            var ranges = line.Split(",").Select(def => ParseRange(def)).ToList();
+            if (ranges[0].Overlaps(ranges[1]))
+            {
+                total++;
+            }
+        }
+        return total;
+    }
+
+    private AssignedRange ParseRange(string def)
+    {
+        var indices = def.Split("-").Select(Int32.Parse).ToList();
+        return new AssignedRange(indices[0], indices[1]);
+    }
+
+    struct AssignedRange
+    {
+        public int Start { get; }
+        public int End { get; }
+
+        public AssignedRange(int start, int end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public bool Overlaps(AssignedRange other)
+        {
+            return Enumerable.Range(Start, End - Start + 1)
+                .Intersect(Enumerable.Range(other.Start, other.End - other.Start + 1))
+                .Any();
+        }
+    }
+}
