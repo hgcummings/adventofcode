eaac8a0587f526c873b8a7439a26413f1029471f
Harry Cummings <harry@xlr3.com>
2022-12-06 19:57:43 +0000

    2022 day 6

---
+++ HGC.AOC.2022/06/Part1.cs
@@ -0,0 +1,35 @@
+﻿using HGC.AOC.Common;
+
+namespace HGC.AOC._2022._06;
+
+public class Part1 : ISolution
+{
+    public object? Answer()
+    {
+        var input = this.GetInputStream("input.txt");
+
+        var index = 0;
+        var buffer = "";
+
+        while (!input.EndOfStream)
+        {
+            var next = (char) input.Read();
+            if (buffer.Length == 4)
+            {
+                buffer = buffer.Substring(1);
+            }
+
+            buffer += next;
+            ++index;
+            if (buffer.Distinct().Count() == 4)
+            {
+                break;
+            }
+
+        }
+        
+        Console.WriteLine(buffer);
+
+        return index;
+    }
+}
---
+++ HGC.AOC.2022/06/Part2.cs
@@ -0,0 +1,35 @@
+﻿using HGC.AOC.Common;
+
+namespace HGC.AOC._2022._06;
+
+public class Part2 : ISolution
+{
+    public object? Answer()
+    {
+        var input = this.GetInputStream("input.txt");
+
+        var index = 0;
+        var buffer = "";
+
+        while (!input.EndOfStream)
+        {
+            var next = (char) input.Read();
+            if (buffer.Length == 14)
+            {
+                buffer = buffer.Substring(1);
+            }
+
+            buffer += next;
+            ++index;
+            if (buffer.Distinct().Count() == 14)
+            {
+                break;
+            }
+
+        }
+        
+        Console.WriteLine(buffer);
+
+        return index;
+    }
+}
