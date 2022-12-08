8ded38cb8933a5218f3d3cc74677308ca3eccbce
Harry Cummings <harry@xlr3.com>
2022-12-08 06:41:59 +0000

    2022 day 8

---
+++ HGC.AOC.2022/08/Part1.cs
@@ -0,0 +1,75 @@
+﻿using System.Text.RegularExpressions;
+using HGC.AOC.Common;
+
+namespace HGC.AOC._2022._08;
+
+public class Part1 : ISolution
+{
+    public object? Answer()
+    {
+        var input = this.ReadInputLines("input.txt");
+
+        var heights = new List<List<int>>();
+        var visible = new List<List<bool>>();
+        
+        foreach (var line in input)
+        {
+            heights.Add(line.Select(c => Int32.Parse("" + c)).ToList());
+            visible.Add(line.Select(_ => false).ToList());
+        }
+
+        for (var y = 0; y < heights.Count; ++y)
+        {
+            var max = -1;
+            for (var x = 0; x < heights[y].Count; ++x)
+            {
+                if (heights[y][x] > max)
+                {
+                    max = heights[y][x];
+                    visible[y][x] = true;
+                }
+            }
+            
+            max = -1;
+            for (var x = heights[y].Count - 1; x >= 0; --x)
+            {
+                if (heights[y][x] > max)
+                {
+                    max = heights[y][x];
+                    visible[y][x] = true;
+                }
+            }
+        }
+        
+        for (var x = 0; x < heights[0].Count; ++x)
+        {
+            var max = -1;
+            for (var y = 0; y < heights.Count; ++y)
+            {
+                if (heights[y][x] > max)
+                {
+                    max = heights[y][x];
+                    visible[y][x] = true;
+                }
+            }
+            
+            max = -1;
+            for (var y = heights.Count - 1; y >= 0; --y)
+            {
+                if (heights[y][x] > max)
+                {
+                    max = heights[y][x];
+                    visible[y][x] = true;
+                }
+            }
+        }
+        
+        // foreach (var row in visible)
+        // {
+        //     Console.WriteLine(String.Concat(row.Select(c => c ? 'V' : ' ')));
+        // }
+
+        return visible.SelectMany(row => row.Where(t => t)).Count();
+    }
+
+}
---
+++ HGC.AOC.2022/08/Part2.cs
@@ -0,0 +1,81 @@
+﻿using System.Text.RegularExpressions;
+using HGC.AOC.Common;
+
+namespace HGC.AOC._2022._08;
+
+public class Part2 : ISolution
+{
+    public object? Answer()
+    {
+        var input = this.ReadInputLines("input.txt");
+
+        var heights = new List<List<int>>();
+        var scores = new List<List<int>>();
+        
+        foreach (var line in input)
+        {
+            heights.Add(line.Select(c => Int32.Parse("" + c)).ToList());
+            scores.Add(line.Select(_ => 1).ToList());
+        }
+
+        for (var y = 0; y < heights.Count; ++y)
+        {
+            for (var x = 0; x < heights[y].Count; ++x)
+            {
+                var height = heights[y][x];
+                
+                var dirScr = 0;
+                for (var i = 1; x + i < heights[y].Count; ++i)
+                {
+                    dirScr += 1;
+                    if (heights[y][x + i] >= height)
+                    {
+                        break;
+                    }
+                }
+                scores[y][x] *= dirScr;
+                
+                dirScr = 0;
+                for (var i = 1; x - i >= 0; ++i)
+                {
+                    dirScr += 1;
+                    if (heights[y][x - i] >= height)
+                    {
+                        break;
+                    }
+                }
+                scores[y][x] *= dirScr;
+                
+                dirScr = 0;
+                for (var j = 1; y + j < heights.Count; ++j)
+                {
+                    dirScr += 1;
+                    if (heights[y + j][x] >= height)
+                    {
+                        break;
+                    }
+                }
+                scores[y][x] *= dirScr;
+                
+                dirScr = 0;
+                for (var j = 1; y - j >= 0; ++j)
+                {
+                    dirScr += 1;
+                    if (heights[y - j][x] >= height)
+                    {
+                        break;
+                    }
+                }
+                scores[y][x] *= dirScr;
+            }
+        }
+        
+        // foreach (var row in scores)
+        // {
+        //     Console.WriteLine(String.Join(',',row));
+        // }
+
+        return scores.SelectMany(row => row).Max();
+    }
+
+}
