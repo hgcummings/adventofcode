978b83eaba5549a55753dece08b81c5427dc2872
Harry Cummings <harry@xlr3.com>
2022-12-11 06:33:22 +0000

    2022 day 11 alt (parsing Operation expression)

---
+++ HGC.AOC.2022/11/Part2Alt.cs
@@ -0,0 +1,89 @@
+﻿using System.Text.RegularExpressions;
+using HGC.AOC.Common;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace HGC.AOC._2022._11;
+
+public class Part2Alt : ISolution
+{
+    public object? Answer()
+    {
+        var input = this.ReadInput("input.txt");
+
+        var monkeyExpr = new Regex("Monkey [0-9]+:\\s*" +
+                                   "Starting items: (?'Items'[0-9, ]+)\\s*" +
+                                   "Operation: new = (?'Operation'.*)\\n\\s*" +
+                                   "Test: divisible by (?'TestFactor'[0-9]+)\\s*" +
+                                    "If true: throw to monkey (?'TrueTarget'[0-9]+)\\s*" +
+                                    "If false: throw to monkey (?'FalseTarget'[0-9]+)"
+                                   );
+
+        var monkeys = monkeyExpr.Matches(input)
+            .Select(match => new Monkey(match.Parse<MonkeyData>())).ToList();
+
+        var commonMultiple = monkeys.Select(m => m.TestFactor).Aggregate((cur, agg) => cur * agg);
+
+        for (var round = 0; round < 10000; ++round)
+        {
+            foreach (var monkey in monkeys)
+            {
+                while (monkey.Items.Count > 0)
+                {
+                    var value = monkey.Items.Dequeue();
+                    monkey.InspectionCount++;
+                    value = monkey.Operation(value);
+                    value = value % commonMultiple;
+                    var target = value % monkey.TestFactor == 0 ? monkey.TrueTarget : monkey.FalseTarget;
+                    monkeys[target].Items.Enqueue(value);
+                }
+            }
+        }
+        
+        var inspectionCounts = monkeys.Select(m => m.InspectionCount).OrderByDescending(x => x).ToList();
+        return (long) inspectionCounts[0] * inspectionCounts[1];
+    }
+
+    public class Monkey
+    {
+        
+        public Monkey(MonkeyData data)
+        {
+            Items = new Queue<long>(data.Items.Split(",").Select(x => long.Parse(x.Trim())));
+            opScript = CSharpScript.Create<long>(data.Operation, globalsType: typeof(OpVars));
+
+            TestFactor = data.TestFactor;
+            TrueTarget = data.TrueTarget;
+            FalseTarget = data.FalseTarget;
+
+            InspectionCount = 0;
+        }
+
+        public long Operation(long old)
+        {
+            return opScript.RunAsync(new OpVars { old = old }).Result.ReturnValue;
+        }
+        
+        public Queue<long> Items { get; }
+        public int TestFactor { get; }
+        public int TrueTarget { get; }
+        public int FalseTarget { get; }
+        public int InspectionCount { get; set; }
+        
+        private Script<long> opScript;
+        
+        public class OpVars {
+            public long old;
+        }
+    }
+
+    public class MonkeyData
+    {
+        public string Items { get; set; }
+        public string Operation { get; set; }
+        public int TestFactor { get; set; }
+        public int TrueTarget { get; set; }
+        public int FalseTarget { get; set; }
+    }
+}
