c5128c79d7947b0f5f20f18e5ef7e938bc4d5182
Harry Cummings <harry@xlr3.com>
2022-12-21 15:39:11 +0000

    2022 day 15

---
+++ HGC.AOC.2022/15/Part1.cs
@@ -0,0 +1,73 @@
+﻿using System.Drawing;
+using System.Text.RegularExpressions;
+using HGC.AOC.Common;
+
+namespace HGC.AOC._2022._15;
+
+public class Part1 : ISolution
+{
+    public object? Answer()
+    {
+        // const int checkRow = 10;
+        // var input = this.ReadInputLines("example.txt");
+
+        const int checkRow = 2000000;
+        var input = this.ReadInputLines("input.txt");
+        
+        var sensorRegex = new Regex("Sensor at x=(?'SensorX'[0-9\\-]+), y=(?'SensorY'[0-9\\-]+): closest beacon is at x=(?'BeaconX'[0-9\\-]+), y=(?'BeaconY'[0-9\\-]+)");
+
+        var sensors = input.Select(line => sensorRegex.Match(line).Parse<SensorData>()).ToList();
+
+        var maxRange = sensors.Select(s => s.BeaconDistance).Max();
+        var maxX = Math.Max(sensors.Select(s => s.SensorX).Max(), sensors.Select(s => s.BeaconX).Max());
+        var maxY = Math.Max(sensors.Select(s => s.SensorY).Max(), sensors.Select(s => s.BeaconY).Max());
+
+        int count = 0;
+        for (var x = -maxRange; x <= maxX + maxRange; ++x)
+        {
+            if (x % 100000 == 0)
+            {
+                Console.WriteLine(x);
+            }
+            // for (var y = -maxRange; y <= maxY + maxRange; ++y)
+            // {
+                var eliminated = CheckEliminated(x, checkRow);
+                if (eliminated)// && y == checkRow)
+                {
+                    count++;
+                }
+            // }
+        }
+
+        bool CheckEliminated(int x, int y)
+        {
+            foreach (var sensor in sensors)
+            {
+                if (sensor.BeaconX == x && sensor.BeaconY == y)
+                {
+                    return false;
+                }
+
+                if (Math.Abs(x - sensor.SensorX) + Math.Abs(y - sensor.SensorY) <= sensor.BeaconDistance)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        return count;
+    }
+
+    private class SensorData
+    {
+        
+        public int SensorX { get; set; }
+        public int SensorY { get; set; }
+        public int BeaconX { get; set; }
+        public int BeaconY { get; set; }
+
+        public int BeaconDistance => Math.Abs(BeaconX - SensorX) + Math.Abs(BeaconY - SensorY);
+    }
+}
---
+++ HGC.AOC.2022/15/Part2.cs
@@ -0,0 +1,84 @@
+﻿using System.Drawing;
+using System.Text.RegularExpressions;
+using HGC.AOC.Common;
+
+namespace HGC.AOC._2022._15;
+
+public class Part2 : ISolution
+{
+    public object? Answer()
+    {
+        var min = 0;
+        var max = 4000000;
+        var input = this.ReadInputLines("input.txt");
+        
+        var sensorRegex = new Regex("Sensor at x=(?'SensorX'[0-9\\-]+), y=(?'SensorY'[0-9\\-]+): closest beacon is at x=(?'BeaconX'[0-9\\-]+), y=(?'BeaconY'[0-9\\-]+)");
+
+        var sensors = input
+            .Select(line => sensorRegex.Match(line).Parse<SensorData>())
+            .OrderByDescending(s => s.BeaconDistance)
+            .ToList();
+
+        for (var y = min; y <= max; ++y)
+        {
+            if (y % 100000 == 0)
+            {
+                Console.WriteLine(y);
+            }
+
+            var ranges = new List<Tuple<int, int>>();
+            foreach (var sensor in sensors)
+            {
+                var rowDist = Math.Abs(y - sensor.SensorY);
+                if (rowDist <= sensor.BeaconDistance)
+                {
+                    var newRange = new Tuple<int, int>(
+                        sensor.SensorX - (sensor.BeaconDistance - rowDist),
+                        sensor.SensorX + (sensor.BeaconDistance - rowDist));
+
+                    var newRanges = new List<Tuple<int, int>>();
+                    foreach (var range in ranges)
+                    {
+                        if (range.Item2 >= newRange.Item1 && range.Item1 <= newRange.Item2)
+                        {
+                            newRange = new Tuple<int, int>(
+                                Math.Min(range.Item1, newRange.Item1),
+                                Math.Max(range.Item2, newRange.Item2));
+                        }
+                        else
+                        {
+                            newRanges.Add(range);
+                        }
+                    }
+
+                    newRanges.Add(newRange);
+                    ranges = newRanges.Distinct().ToList();
+                }
+            }
+
+            if (ranges.Count > 1)
+            {
+                Console.Write(y + ": ");
+                foreach (var range in ranges.OrderBy(range => range.Item1))
+                {
+                    Console.Write(range);
+                }
+                Console.WriteLine();
+                return ((ranges.OrderBy(range => range.Item1).First().Item2 + 1) * (long) 4000000) + y;
+            }
+        }
+
+        return null;
+    }
+
+    private class SensorData
+    {
+        
+        public int SensorX { get; set; }
+        public int SensorY { get; set; }
+        public int BeaconX { get; set; }
+        public int BeaconY { get; set; }
+
+        public int BeaconDistance => Math.Abs(BeaconX - SensorX) + Math.Abs(BeaconY - SensorY);
+    }
+}
