0068f2912cd15cd0fcfd38ad5184285ae530ecd7
Harry Cummings <harry@xlr3.com>
2023-12-03 07:38:17 +0000

    2023 day 03

---
+++ HGC.AOC.2023/03/Part1.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.RegularExpressions;
+using HGC.AOC.Common;
+
+namespace HGC.AOC._2023._03;
+
+public class Part1 : ISolution
+{
+    public object? Answer()
+    {
+        var lines = this.ReadInputLines("input.txt").ToArray();
+        var numRegex = new Regex("[0-9]+");
+
+        var partSum = 0;
+
+        IEnumerable<string> Neighbours(int line, Match location)
+        {
+            var minX = Math.Max(0, location.Index - 1);
+            var maxX = Math.Min(location.Index + location.Length + 1, lines[line].Length);
+            var maxLength = maxX - minX;
+            if (line > 0)
+            {
+                yield return lines[line - 1].Substring(minX, maxLength);
+            }
+
+            if (location.Index > 0)
+            {
+                yield return lines[line].Substring(location.Index - 1, 1);
+            }
+
+            if (location.Index + location.Length < lines[line].Length)
+            {
+                yield return lines[line].Substring(location.Index + location.Length, 1);
+            }
+
+            if (line < lines.Length - 1)
+            {
+                yield return lines[line + 1].Substring(minX, maxLength);
+            }
+        }
+
+        for (int y = 0; y < lines.Length; ++y)
+        {
+            var line = lines[y];
+            var matches = numRegex.Matches(line);
+
+            foreach (Match match in matches)
+            {
+                if (Neighbours(y, match).Any(n => n.Any(c => c != '.')))
+                {
+                    partSum += Int32.Parse(match.Value);
+                }
+            }
+        }
+
+        return partSum;
+    }
+}
---
+++ HGC.AOC.2023/03/Part2.cs
@@ -0,0 +1,79 @@
+﻿using System.Drawing;
+using System.Text.RegularExpressions;
+using HGC.AOC.Common;
+
+namespace HGC.AOC._2023._03;
+
+public class Part2 : ISolution
+{
+    public object? Answer()
+    {
+        var lines = this.ReadInputLines("input.txt").ToArray();
+        var numRegex = new Regex("[0-9]+");
+
+        var gearLocations = new Dictionary<Point, List<int>>();
+
+        IEnumerable<Point> GearNeighbours(int line, Match location)
+        {
+            var minX = Math.Max(0, location.Index - 1);
+            var maxX = Math.Min(location.Index + location.Length + 1, lines[line].Length);
+            if (line > 0)
+            {
+                for (var x = minX; x < maxX; ++x)
+                {
+                    if (lines[line - 1][x] == '*')
+                    {
+                        yield return new Point(x, line - 1);
+                    }
+                }
+            }
+
+            if (location.Index > 0 && lines[line][location.Index - 1] == '*')
+            {
+                yield return new Point(location.Index - 1, line);
+            }
+
+            if ((location.Index + location.Length < lines[line].Length) && lines[line][location.Index + location.Length] == '*')
+            {
+                yield return new Point(location.Index + location.Length, line);
+            }
+
+            if (line < lines.Length - 1)
+            {
+                for (var x = minX; x < maxX; ++x)
+                {
+                    if (lines[line + 1][x] == '*')
+                    {
+                        yield return new Point(x, line + 1);
+                    }
+                }
+            }
+        }
+
+        for (int y = 0; y < lines.Length; ++y)
+        {
+            var line = lines[y];
+            var matches = numRegex.Matches(line);
+
+            foreach (Match match in matches)
+            {
+                foreach (var gear in GearNeighbours(y, match))
+                {
+                    if (gearLocations.ContainsKey(gear))
+                    {
+                        gearLocations[gear].Add(Int32.Parse(match.Value));
+                    }
+                    else
+                    {
+                        gearLocations[gear] = new List<int> { Int32.Parse(match.Value) };
+                    }
+                }
+            }
+        }
+
+        return gearLocations
+            .Where(g => g.Value.Count == 2)
+            .Select(g => g.Value[0] * g.Value[1])
+            .Sum();
+    }
+}
