5e4268ea5b5cb173fc30d33163d4edb37e9b24e7
Harry Cummings <harry@xlr3.com>
2023-12-04 06:59:52 +0000

    2023 day 04

---
+++ HGC.AOC.2023/04/Part1.cs
@@ -0,0 +1,26 @@
+﻿using HGC.AOC.Common;
+
+namespace HGC.AOC._2023._04;
+
+public class Part1 : ISolution
+{
+    public object? Answer()
+    {
+        var cards = this.ReadInputLines("input.txt");
+
+        var total = cards.Select(card =>
+        {
+            var winningNumbers = card.Split("|")[0].Split(":")[1].Trim()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse);
+            var haveNumbers = card.Split("|")[1].Trim()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse);
+
+            var winCount = winningNumbers.Intersect(haveNumbers).Count();
+
+            var score = winCount == 0 ? 0 : Math.Pow(2, winCount - 1);
+            return score;
+        }).Sum();
+        
+        return total;
+    }
+}
---
+++ HGC.AOC.2023/04/Part2.cs
@@ -0,0 +1,29 @@
+﻿using HGC.AOC.Common;
+
+namespace HGC.AOC._2023._04;
+
+public class Part2 : ISolution
+{
+    public object? Answer()
+    {
+        var cards = this.ReadInputLines("input.txt").ToArray();
+        var wins = ArrayHelpers.InitArray(cards.Length, _ => 1);
+
+        for (int i = 0; i < cards.Length; ++i)
+        {
+            var winningNumbers = cards[i].Split("|")[0].Split(":")[1].Trim()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse);
+            var haveNumbers = cards[i].Split("|")[1].Trim()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse);
+
+            var winCount = winningNumbers.Intersect(haveNumbers).Count();
+
+            for (int j = i + 1; j <= i + winCount && j < wins.Length; ++j)
+            {
+                wins[j] += wins[i];
+            }
+        }
+        
+        return wins.Sum();
+    }
+}
