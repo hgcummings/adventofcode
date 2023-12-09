927b653b39971b593a8282e6456c2d56384ffa50
Harry Cummings <harry@xlr3.com>
2023-12-09 06:53:58 +0000

    2023 day 09

---
+++ HGC.AOC.2023/09/Part1.cs
@@ -0,0 +1,29 @@
+﻿using HGC.AOC.Common;
+
+namespace HGC.AOC._2023._09;
+
+public class Part1 : ISolution
+{
+    public object? Answer()
+    {
+        return this.ReadInputLines("input.txt")
+            .Select(line => line.SplitBySpaces().Select(Int32.Parse))
+            .Select(seq =>
+            {
+                var currentSeq = seq.ToList();
+                var lastValues = new List<int> { currentSeq.Last() };
+                while (currentSeq.Any(x => x != 0))
+                {
+                    var newSeq = new List<int>();
+                    for (var i = 0; i < currentSeq.Count - 1; ++i)
+                    {
+                        newSeq.Add(currentSeq[i+1]-currentSeq[i]);
+                    }
+                    lastValues.Add(newSeq.Last());
+                    currentSeq = newSeq;
+                }
+
+                return lastValues.Sum();
+            }).Sum();
+    }
+}
---
+++ HGC.AOC.2023/09/Part2.cs
@@ -0,0 +1,31 @@
+﻿using HGC.AOC.Common;
+
+namespace HGC.AOC._2023._09;
+
+public class Part2 : ISolution
+{
+    public object? Answer()
+    {
+        return this.ReadInputLines("input.txt")
+            .Select(line => line.SplitBySpaces().Select(Int32.Parse))
+            .Select(seq =>
+            {
+                var currentSeq = seq.ToList();
+                var firstValues = new List<int> { currentSeq.First() };
+                while (currentSeq.Any(x => x != 0))
+                {
+                    var newSeq = new List<int>();
+                    for (var i = 0; i < currentSeq.Count - 1; ++i)
+                    {
+                        newSeq.Add(currentSeq[i+1]-currentSeq[i]);
+                    }
+                    firstValues.Add(newSeq.First());
+                    currentSeq = newSeq;
+                }
+
+                firstValues.Reverse();
+                var result = firstValues.Aggregate((acc, curr) => curr - acc);
+                return result;
+            }).Sum();
+    }
+}
