0d6cb1c44bb98417bc55e44557c751ae38ddeca2
Harry Cummings <harry@xlr3.com>
2023-12-11 07:25:18 +0000

    2023 day 11

---
+++ HGC.AOC.2023/11/Part1.cs
@@ -0,0 +1,77 @@
+﻿using System.Drawing;
+using HGC.AOC.Common;
+
+namespace HGC.AOC._2023._11;
+
+public class Part1 : ISolution
+{
+    public object? Answer()
+    {
+        var input = this.ReadInputLines("input.txt");
+
+        var map = new List<String>();
+        List<int> emptyCols = null;
+        foreach (var line in input)
+        {
+            if (emptyCols == null)
+            {
+                emptyCols = Enumerable.Range(0, line.Length).ToList();
+            }
+            
+            map.Add(line);
+            if (line.All(c => c == '.'))
+            {
+                map.Add(line);
+            }
+
+            for (var y = 0; y < line.Length; ++y)
+            {
+                if (line[y] != '.')
+                {
+                    emptyCols.Remove(y);
+                }
+            }
+        }
+
+
+        var count = 0;
+        foreach (var i in emptyCols)
+        {
+            for (var x = 0; x < map.Count; ++x)
+            {
+                map[x] = map[x].Insert(i + count, ".");
+            }
+
+            ++count;
+        }
+
+        var galaxies = new List<Point>();
+        for (var x = 0; x < map.Count; ++x)
+        {
+            var row = map[x];
+            for (var y = 0; y < row.Length; ++y)
+            {
+                if (row[y] == '#')
+                {
+                    galaxies.Add(new Point(x,y));
+                }
+            }
+        }
+
+        var totalDistance = 0;
+
+        for (var i = 0; i < galaxies.Count; ++i)
+        {
+            for (var j = i + 1; j < galaxies.Count; ++j)
+            {
+                var a = galaxies[i];
+                var b = galaxies[j];
+
+                var distance = Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
+                totalDistance += distance;
+            }
+        }
+
+        return totalDistance;
+    }
+}
---
+++ HGC.AOC.2023/11/Part2.cs
@@ -0,0 +1,70 @@
+﻿using System.Drawing;
+using HGC.AOC.Common;
+
+namespace HGC.AOC._2023._11;
+
+public class Part2 : ISolution
+{
+    public object? Answer()
+    {
+        var map = this.ReadInputLines("input.txt").ToArray();
+
+        List<int> emptyCols = null;
+        List<int> emptyRows = new List<int>();
+        var rowIndex = 0;
+        foreach (var line in map)
+        {
+            if (emptyCols == null)
+            {
+                emptyCols = Enumerable.Range(0, line.Length).ToList();
+            }
+            
+            if (line.All(c => c == '.'))
+            {
+                emptyRows.Add(rowIndex);
+            }
+
+            for (var y = 0; y < line.Length; ++y)
+            {
+                if (line[y] != '.')
+                {
+                    emptyCols.Remove(y);
+                }
+            }
+
+            rowIndex++;
+        }
+
+        var galaxies = new List<Point>();
+        for (var x = 0; x < map.Length; ++x)
+        {
+            var row = map[x];
+            for (var y = 0; y < row.Length; ++y)
+            {
+                if (row[y] == '#')
+                {
+                    galaxies.Add(new Point(x,y));
+                }
+            }
+        }
+
+        long totalDistance = 0;
+
+        for (var i = 0; i < galaxies.Count; ++i)
+        {
+            for (var j = i + 1; j < galaxies.Count; ++j)
+            {
+                var a = galaxies[i];
+                var b = galaxies[j];
+
+                long distance = Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y)
+                    + 999999 * emptyRows.Count(c => c < Math.Max(a.X, b.X) && c > Math.Min(a.X, b.X))
+                    + 999999 * emptyCols.Count(c => c < Math.Max(a.Y, b.Y) && c > Math.Min(a.Y, b.Y));
+                
+                totalDistance += distance;
+            }
+        }
+
+        return totalDistance;
+    }
+}
