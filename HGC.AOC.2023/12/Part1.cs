954e5b407c916a416a9ba4880deaf0d5cf0c31e8
Harry Cummings <harry@xlr3.com>
2023-12-13 18:43:04 +0000

    2023 day 12 (optimised)

--- HGC.AOC.2023/12/Part1.cs
+++ HGC.AOC.2023/12/Part1.cs
@@ -11,82 +11,86 @@ public class Part1 : ISolution
             .Sum(PossibleArrangements);
     }
 
-     struct Record
-     {
-         public string Springs { get; }
-         public List<int> Groups { get; }
+    struct Record
+    {
+        public string Springs { get; }
+        public int[] Groups { get; }
 
-         public Record(string springs, List<int> groups)
-         {
-             Springs = springs;
-             Groups = groups;
-         }
+        public Record(string springs, int[] groups)
+        {
+            Springs = springs;
+            Groups = groups;
+        }
          
-         public Record(string line) {
-             var components = line.SplitBySpaces();
-             var springs = components[0];
-             var groups = components[1].Split(",").Select(Int32.Parse).ToList();
+        public Record(string line) {
+            var components = line.SplitBySpaces();
+            var springs = components[0];
+            var groups = components[1].Split(",").Select(Int32.Parse).ToArray();
          
-             Springs = springs;
-             Groups = groups;
-         }
+            Springs = springs;
+            Groups = groups;
+        }
 
-         public override string ToString()
-         {
-             return $"{Springs} {String.Join(",", Groups)}";
-         }
-     }
+        public override string ToString()
+        {
+            return $"{Springs} {String.Join(",", Groups)}";
+        }
+    }
 
-     int PossibleArrangements(Record record)
-     {
-         if (record.Groups.Count == 0)
-         {
-             return record.Springs.Any(c => c == '#') ? 0 : 1;
-         }
+    int PossibleArrangements(Record record)
+    {
+        return PossibleArrangements(record.Springs, record.Groups);
+    }
+    
+    int PossibleArrangements(ReadOnlySpan<char> springs, ReadOnlySpan<int> groups)
+    {
+        if (groups.Length == 0)
+        {
+            return springs.Contains('#') ? 0 : 1;
+        }
 
-         var size = record.Groups[0];
-         var sp = record.Springs;
-         var total = 0;
-         for (var i = 0; i <= sp.Length - size; ++i)
-         {
-             if (sp[i] == '.')
-             {
-                 continue;
-             }
+        var size = groups[0];
+        var total = 0;
+        for (var i = 0; i <= springs.Length - size; ++i)
+        {
+            if (springs[i] == '.')
+            {
+                continue;
+            }
 
-             var canFit = true;
-             for (var j = 0; j < size; ++j)
-             {
-                 if (sp[i + j] == '.')
-                 {
-                     canFit = false;
-                     break;
-                 }
-             }
+            var canFit = true;
+            for (var j = 0; j < size; ++j)
+            {
+                if (springs[i + j] == '.')
+                {
+                    canFit = false;
+                    break;
+                }
+            }
 
-             if (canFit)
-             {
-                 if (i + size < sp.Length)
-                 {
-                     if (sp[i + size] != '#')
-                     {
-                         total += PossibleArrangements(new Record(
-                             sp.Substring(i + size + 1),
-                             record.Groups.Skip(1).ToList()));
-                     }
-                 }
-                 else
-                 {
-                     total += record.Groups.Count == 1 ? 1 : 0;
-                 }
-             }
+            if (canFit)
+            {
+                if (i + size < springs.Length)
+                {
+                    if (springs[i + size] != '#')
+                    {
+                        total += PossibleArrangements(
+                            springs[(i + size + 1)..],
+                            groups[1..]);
+                    }
+                }
+                else
+                {
+                    total += groups.Length == 1 ? 1 : 0;
+                }
+            }
 
-             if (sp[i] == '#')
-             {
-                 break;
-             }
-         }
+            if (springs[i] == '#')
+            {
+                break;
+            }
+        }
 
-         return total;
-     }
+        return total;
+    }
 }
--- HGC.AOC.2023/12/Part2.cs
+++ HGC.AOC.2023/12/Part2.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
 using HGC.AOC.Common;
 
 namespace HGC.AOC._2023._12;
@@ -7,102 +8,109 @@ public class Part2 : ISolution
 {
     public object? Answer()
     {
-        var completion = 0;
         return this.ReadInputLines("input.txt")
             .Select(line => new Record(line))
             .Select(record => new Record(
                 String.Join("?", Enumerable.Repeat(record.Springs, 5)),
                 String.Join(",", Enumerable.Repeat(String.Join(",", record.Groups), 5))))
-             .AsParallel()
-             .Sum(record =>
-             {
-                 var stopwatch = new Stopwatch();
-                 stopwatch.Start();
-                 var count = PossibleArrangements(record.Springs.AsSpan(), record.Groups);
-                 stopwatch.Stop();
-                 Console.Write(Interlocked.Increment(ref completion));
-                 Console.Write($" ({count} in {stopwatch.ElapsedMilliseconds/1000}s) ");
-                 Console.Write(record.Springs.Substring(0, record.Springs.Length / 5) + " ");
-                 Console.WriteLine(String.Join(",", record.Groups.Take(record.Groups.Length / 5)));
-                 return count;
-             });
+            .AsParallel()
+            .Sum(record => record.PossibleArrangements());
     }
 
-     struct Record
-     {
-         public string Springs { get; }
-         public int[] Groups { get; }
+    class Record
+    {
+        public string Springs { get; }
+        public int[] Groups { get; }
          
-         public Record(string springs, string groups)
-         {
-             Springs = springs;
-             Groups = groups.Split(",").Select(Int32.Parse).ToArray();
-         }
+        public Record(string springs, string groups)
+        {
+            Springs = springs;
+            Groups = groups.Split(",").Select(Int32.Parse).ToArray();
+        }
          
-         public Record(string line) {
-             var components = line.SplitBySpaces();
-             var springs = components[0];
-             var groups = components[1].Split(",").Select(Int32.Parse).ToArray();
+        public Record(string line) {
+            var components = line.SplitBySpaces();
+            var springs = components[0];
+            var groups = components[1].Split(",").Select(Int32.Parse).ToArray();
          
-             Springs = springs;
-             Groups = groups;
-         }
+            Springs = springs;
+            Groups = groups;
+        }
 
-         public override string ToString()
-         {
-             return $"{Springs} {String.Join(",", Groups)}";
-         }
-     }
+        public override string ToString()
+        {
+            return $"{Springs} {String.Join(",", Groups)}";
+        }
 
-     long PossibleArrangements(ReadOnlySpan<char> sp, ReadOnlySpan<int> groups)
-     {
-         if (groups.Length == 0)
-         {
-             return sp.Contains('#') ? 0 : 1;
-         }
+        public long PossibleArrangements()
+        {
+            return PossibleArrangements(0, 0);
+        }
 
-         var size = groups[0];
-         long total = 0;
-         for (var i = 0; i <= sp.Length - size; ++i)
-         {
-             if (sp[i] == '.')
-             {
-                 continue;
-             }
+        private readonly ConcurrentDictionary<(int sOff, int gOff), long> cache = new();
 
-             var canFit = true;
-             for (var j = 0; j < size; ++j)
-             {
-                 if (sp[i + j] == '.')
-                 {
-                     canFit = false;
-                     break;
-                 }
-             }
+        long PossibleArrangements(int springsOffset, int groupsOffset)
+        {
+            return cache.GetOrAdd(
+                (springsOffset, groupsOffset),
+                key => PossibleArrangementsRaw(key.sOff, key.gOff));
+        }
+         
+        long PossibleArrangementsRaw(int springsOffset, int groupsOffset)
+        {
+            var sp = Springs[springsOffset..];
+            var groups = Groups[groupsOffset..];
+              
+            if (groups.Length == 0)
+            {
+                return sp.Contains('#') ? 0 : 1;
+            }
+     
+            var size = groups[0];
+            long total = 0;
+            for (var i = 0; i <= sp.Length - size; ++i)
+            {
+                if (sp[i] == '.')
+                {
+                    continue;
+                }
+     
+                var canFit = true;
+                for (var j = 0; j < size; ++j)
+                {
+                    if (sp[i + j] == '.')
+                    {
+                        canFit = false;
+                        break;
+                    }
+                }
+     
+                if (canFit)
+                {
+                    if (i + size < sp.Length)
+                    {
+                        if (sp[i + size] != '#')
+                        {
+                            total += PossibleArrangements(
+                                springsOffset + i + size + 1,
+                                groupsOffset + 1);
+                        }
+                    }
+                    else
+                    {
+                        total += groups.Length == 1 ? 1 : 0;
+                    }
+                }
+     
+                if (sp[i] == '#')
+                {
+                    break;
+                }
+            }
+     
+            return total;
+        }
+    }
 
-             if (canFit)
-             {
-                 if (i + size < sp.Length)
-                 {
-                     if (sp[i + size] != '#')
-                     {
-                         total += PossibleArrangements(
-                             sp[(i + size + 1)..],
-                             groups[1..]);
-                     }
-                 }
-                 else
-                 {
-                     total += groups.Length == 1 ? 1 : 0;
-                 }
-             }
-
-             if (sp[i] == '#')
-             {
-                 break;
-             }
-         }
-
-         return total;
-     }
+     
 }
