c6378661f6f51390e737244f84458d7263de023b
Harry Cummings <harry@xlr3.com>
2023-12-16 08:41:42 +0000

    2023 day 16 tidy-ups

--- HGC.AOC.2023/16/Part1.cs
+++ HGC.AOC.2023/16/Part1.cs
@@ -1,5 +1,4 @@
 ﻿using System.Drawing;
-using System.Runtime.Intrinsics.X86;
 using HGC.AOC.Common;
 
 namespace HGC.AOC._2023._16;
@@ -12,65 +11,42 @@ public class Part1 : ISolution
         var beams = new List<Beam> { new(0, 0, Dir.Right) };
         var energisedLocations = new HashSet<Point>();
         var processedBeamStates = new HashSet<Beam>();
-        var history = new List<int>();
 
-        var beamsToProcess = true;
-        while (beamsToProcess)
+        while (beams.Count > 0)
         {
             var newBeams = new List<Beam>();
-            beamsToProcess = false;
             foreach (var beam in beams)
             {
-                if (!beam.InBounds(map) || processedBeamStates.Contains(beam))
+                if (beam.InBounds(map) && processedBeamStates.Add(beam))
                 {
-                    continue;
-                }
-
-                beamsToProcess = true;
-                processedBeamStates.Add(beam);
-                
-                var cell = map[beam.Y][beam.X];
-                energisedLocations.Add(new Point(beam.X, beam.Y));
-                switch (cell, beam.D)
-                {
-                    case ('.', _) or ('-', Dir.Right) or ('|', Dir.Down) or ('-', Dir.Left) or ('|', Dir.Up):
-                        newBeams.Add(beam.Advance());
-                        break;
-                    case ('/', _) or ('\\', _):
-                        newBeams.Add(beam.Reflect(cell).Advance());
-                        break;
-                    case ('|', Dir.Right) or ('|', Dir.Left):
-                        newBeams.Add(new Beam(beam.X, beam.Y + 1, Dir.Down));
-                        newBeams.Add(new Beam(beam.X, beam.Y - 1, Dir.Up));
-                        break;
-                    case ('-', Dir.Down) or ('-', Dir.Up):
-                        newBeams.Add(new Beam(beam.X + 1, beam.Y, Dir.Right));
-                        newBeams.Add(new Beam(beam.X - 1, beam.Y, Dir.Left));
-                        break;
+                    var cell = map[beam.Y][beam.X];
+                    energisedLocations.Add(new Point(beam.X, beam.Y));
+                    switch (cell, beam.D)
+                    {
+                        case ('.', _)
+                            or ('-', Dir.Right) or ('|', Dir.Down)
+                            or ('-', Dir.Left) or ('|', Dir.Up):
+                            newBeams.Add(beam.Advance());
+                            break;
+                        case ('/', _) or ('\\', _):
+                            newBeams.Add(beam.Reflect(cell).Advance());
+                            break;
+                        case ('|', Dir.Right) or ('|', Dir.Left):
+                            newBeams.Add(new Beam(beam.X, beam.Y + 1, Dir.Down));
+                            newBeams.Add(new Beam(beam.X, beam.Y - 1, Dir.Up));
+                            break;
+                        case ('-', Dir.Down) or ('-', Dir.Up):
+                            newBeams.Add(new Beam(beam.X + 1, beam.Y, Dir.Right));
+                            newBeams.Add(new Beam(beam.X - 1, beam.Y, Dir.Left));
+                            break;
+                    }
                 }
             }
 
             beams = newBeams;
-            history.Add(energisedLocations.Count);
-            Console.WriteLine(energisedLocations.Count);
         }
 
-        // OutputEnergised(map, energisedLocations);
-        return history[^1];
-    }
-
-    private static void OutputEnergised(string[] map, HashSet<Point> energisedLocations)
-    {
-        Console.WriteLine();
-        for (var y = 0; y < map.Length; ++y)
-        {
-            for (var x = 0; x < map[y].Length; ++x)
-            {
-                Console.Write(energisedLocations.Contains(new Point(x, y)) ? '#' : '.');
-            }
-            Console.WriteLine();
-        }
-        Console.WriteLine();
+        return energisedLocations.Count;
     }
 
     struct Beam(int x, int y, Dir d)
@@ -96,36 +72,26 @@ public class Part1 : ISolution
 
         public Beam Advance()
         {
-            switch (D)
+            return D switch
             {
-                case Dir.Right:
-                    return new Beam(X + 1, Y, D);
-                case Dir.Down:
-                    return new Beam(X, Y + 1, D);
-                case Dir.Left:
-                    return new Beam(X - 1, Y, D);
-                case Dir.Up:
-                    return new Beam(X, Y - 1, D);
-            }
-
-            throw new Exception();
+                Dir.Right => new Beam(X + 1, Y, D),
+                Dir.Down => new Beam(X, Y + 1, D),
+                Dir.Left => new Beam(X - 1, Y, D),
+                Dir.Up => new Beam(X, Y - 1, D),
+                _ => throw new Exception()
+            };
         }
 
         public Beam Reflect(char cell)
         {
-            switch (D, cell)
+            return (D, cell) switch
             {
-                case (Dir.Down, '\\') or (Dir.Up, '/'):
-                    return new Beam(X, Y, Dir.Right);
-                case (Dir.Right, '\\') or (Dir.Left, '/'):
-                    return new Beam(X, Y, Dir.Down);
-                case (Dir.Down, '/') or (Dir.Up, '\\'):
-                    return new Beam(X, Y, Dir.Left);
-                case (Dir.Right, '/') or (Dir.Left, '\\'):
-                    return new Beam(X, Y, Dir.Up);
-            }
-            
-            throw new Exception();
+                (Dir.Down, '\\') or (Dir.Up, '/') => new Beam(X, Y, Dir.Right),
+                (Dir.Right, '\\') or (Dir.Left, '/') => new Beam(X, Y, Dir.Down),
+                (Dir.Down, '/') or (Dir.Up, '\\') => new Beam(X, Y, Dir.Left),
+                (Dir.Right, '/') or (Dir.Left, '\\') => new Beam(X, Y, Dir.Up),
+                _ => throw new Exception()
+            };
         }
 
         public bool InBounds(string[] map)
