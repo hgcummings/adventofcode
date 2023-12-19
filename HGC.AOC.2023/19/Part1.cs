844f6b109017be01ed2c61ea7584bbdc1c2a5292
Harry Cummings <harry@xlr3.com>
2023-12-19 08:38:23 +0000

    2023 day 19

---
+++ HGC.AOC.2023/19/Part1.cs
@@ -0,0 +1,153 @@
+﻿using System.Drawing;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+using HGC.AOC.Common;
+
+namespace HGC.AOC._2023._19;
+
+public class Part1 : ISolution
+{
+    public object? Answer()
+    {
+
+        var input = this.ReadInputLines("input.txt");
+
+        var workflows = new Dictionary<string, Rule[]>();
+        var parts = new List<Part>();
+        
+        using (var inputEnumerator = input.GetEnumerator())
+        {
+            var workflowRegex = new Regex(@"(?'Name'[a-z]+)\{(?'Rules'.*)\}");
+            while (inputEnumerator.MoveNext())
+            {
+                var line = inputEnumerator.Current;
+                if (line.Trim() == "")
+                {
+                    break;
+                }
+
+                var workflowData = workflowRegex.Match(line).Parse<WorkflowData>();
+                var rules = workflowData.Rules.Split(",").Select(ParseRule).ToArray();
+
+                workflows[workflowData.Name] = rules;
+            }
+
+            var partRegex =
+                new Regex(@"{x=(?'X'[0-9]+),m=(?'M'[0-9]+),a=(?'A'[0-9]+),s=(?'S'[0-9]+)}");
+            while (inputEnumerator.MoveNext())
+            {
+                var line = inputEnumerator.Current;
+                if (line.Trim() == "")
+                {
+                    break;
+                }
+
+                var partData = partRegex.Match(line).Parse<PartData>();
+                parts.Add(new Part(partData.X, partData.M, partData.A, partData.S));
+            }
+        }
+
+        var accepted = new List<Part>();
+        foreach (var part in parts)
+        {
+            var destination = "in";
+            while (!(destination == "R" || destination == "A"))
+            {
+                var workflow = workflows[destination];
+                foreach (var rule in workflow)
+                {
+                    if (rule.Condition(part))
+                    {
+                        destination = rule.Destination;
+                        break;
+                    }
+                }
+            }
+
+            if (destination == "A")
+            {
+                accepted.Add(part);
+            }
+        }
+
+
+        return accepted.Select(part => part.X + part.M + part.A + part.S).Sum();
+    }
+
+    public class WorkflowData
+    {
+        public string Name { get; set; }
+        public string Rules { get; set; }
+    }
+
+    public Rule ParseRule(string rule)
+    {
+        var parts = rule.Split(":");
+        if (parts.Length == 1)
+        {
+            return new Rule(_ => true, parts[0]);
+        }
+
+        var conditionData = ConditionRegex.Match(parts[0]).Parse<ConditionData>();
+        
+        var param = Expression.Parameter(typeof(Part), "p");
+        var member = Expression.Property(param, conditionData.Property);
+        var constant = Expression.Constant(conditionData.Value);
+        var body = Expression.MakeBinary(
+            conditionData.Operator switch
+            {
+                ">" => ExpressionType.GreaterThan,
+                "<" => ExpressionType.LessThan,
+                _ => throw new Exception("Unrecognised rule type")
+            },
+            member, constant);
+       
+        return new Rule(Expression.Lambda<Func<Part, bool>>(body, param).Compile(), parts[1]);
+    }
+
+    private static Regex ConditionRegex =
+        new Regex(@"(?'Property'[xmas])(?'Operator'[<>])(?'Value'[0-9]+)");
+
+    public class ConditionData
+    {
+        public string Property { get; set; }
+        public string Operator { get; set; }
+        public int Value { get; set; }
+    }
+
+    public class Rule
+    {
+        public Rule(Func<Part, bool> condition, string destination)
+        {
+            Condition = condition;
+            Destination = destination;
+        }
+
+        public Func<Part, bool> Condition { get; }
+        public string Destination { get; }
+    }
+
+    public struct Part
+    {
+        public Part(int x, int m, int a, int s)
+        {
+            X = x;
+            M = m;
+            A = a;
+            S = s;
+        }
+
+        public int X { get; }
+        public int M { get; }
+        public int A { get; }
+        public int S { get; }
+    }
+
+    public class PartData
+    {
+        public int X { get; set; }
+        public int M { get; set; }
+        public int A { get; set; }
+        public int S { get; set; }
+    }
+}
---
+++ HGC.AOC.2023/19/Part2.cs
@@ -0,0 +1,202 @@
+﻿using System.Drawing;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+using HGC.AOC.Common;
+
+namespace HGC.AOC._2023._19;
+
+public class Part2 : ISolution
+{
+    public object? Answer()
+    {
+
+        var inputLines = this.ReadInputLines("input.txt");
+
+        var workflows = new Dictionary<string, Rule[]>();
+        
+        using (var inputEnumerator = inputLines.GetEnumerator())
+        {
+            var workflowRegex = new Regex(@"(?'Name'[a-z]+)\{(?'Rules'.*)\}");
+            while (inputEnumerator.MoveNext())
+            {
+                var line = inputEnumerator.Current;
+                if (line.Trim() == "")
+                {
+                    break;
+                }
+
+                var workflowData = workflowRegex.Match(line).Parse<WorkflowData>();
+                var rules = workflowData.Rules.Split(",").Select(ParseRule).ToArray();
+
+                workflows[workflowData.Name] = rules;
+            }
+        }
+
+        var accepted = new List<PartRange>();
+        var fullRange = new PartRange(
+            new Range(1, 4001), new Range(1, 4001), new Range(1, 4001), new Range(1, 4001));
+        var inProgress = new Stack<(string location, PartRange range)>();
+        inProgress.Push(("in", fullRange));
+
+        while (inProgress.TryPop(out var input))
+        {
+            var workflow = workflows[input.location];
+            PartRange? remainingRange = input.range;
+            foreach (var rule in workflow)
+            {
+                (var matchingRange, remainingRange) = Split(remainingRange.Value, rule.Condition);
+                if (rule.Destination == "A")
+                {
+                    accepted.Add(matchingRange);
+                }
+                else if (rule.Destination != "R")
+                {
+                    inProgress.Push((rule.Destination, matchingRange));
+                }
+
+                if (remainingRange == null)
+                {
+                    break;
+                }
+            }
+        }
+
+        return accepted.Sum(partRange =>
+            Size(partRange.X) * Size(partRange.M) * Size(partRange.A) * Size(partRange.S));
+    }
+
+    public (PartRange matchingRange, PartRange? remainingRange) 
+        Split(PartRange range, ConditionData? condition)
+    {
+        if (condition == null)
+        {
+            return (range, null);
+        }
+
+        return condition.Property switch
+        {
+            "x" => (
+                new PartRange(Include(range.X, condition), range.M, range.A, range.S),
+                new PartRange(Exclude(range.X, condition), range.M, range.A, range.S)),
+            "m" => (
+                new PartRange(range.X, Include(range.M, condition), range.A, range.S),
+                new PartRange(range.X, Exclude(range.M, condition), range.A, range.S)),
+            "a" => (
+                new PartRange(range.X, range.M, Include(range.A, condition), range.S),
+                new PartRange(range.X, range.M, Exclude(range.A, condition), range.S)),
+            "s" => (
+                new PartRange(range.X, range.M, range.A, Include(range.S, condition)),
+                new PartRange(range.X, range.M, range.A, Exclude(range.S, condition))),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+
+    public Range Include(Range range, ConditionData condition)
+    {
+        if (condition.Operator == "<")
+        {
+            if (range.End.Value <= condition.Value)
+            {
+                return range;
+            }
+            return new Range(range.Start, condition.Value);
+        }
+
+        if (condition.Operator == ">")
+        {
+            if (range.Start.Value > condition.Value)
+            {
+                return range;
+            }
+
+            return new Range(condition.Value + 1, range.End);
+        }
+
+        throw new Exception("Unrecognised rule");
+    }
+    
+    public Range Exclude(Range range, ConditionData condition)
+    {
+        if (condition.Operator == "<")
+        {
+            if (range.Start.Value >= condition.Value)
+            {
+                return range;
+            }
+
+            return new Range(condition.Value, range.End);
+        }
+
+        if (condition.Operator == ">")
+        {
+            if (range.End.Value <= condition.Value)
+            {
+                return range;
+            }
+            return new Range(range.Start, condition.Value + 1);
+        }
+
+        throw new Exception("Unrecognised rule");
+    }
+
+    public long Size(Range range)
+    {
+        return range.End.Value - range.Start.Value;
+    }
+
+    public class WorkflowData
+    {
+        public string Name { get; set; }
+        public string Rules { get; set; }
+    }
+
+    public Rule ParseRule(string rule)
+    {
+        var parts = rule.Split(":");
+        if (parts.Length == 1)
+        {
+            return new Rule(null, parts[0]);
+        }
+
+        var conditionData = ConditionRegex.Match(parts[0]).Parse<ConditionData>();
+        return new Rule(conditionData, parts[1]);
+    }
+
+    private static Regex ConditionRegex =
+        new Regex(@"(?'Property'[xmas])(?'Operator'[<>])(?'Value'[0-9]+)");
+
+    public class ConditionData
+    {
+        public string Property { get; set; }
+        public string Operator { get; set; }
+        public int Value { get; set; }
+    }
+
+    public class Rule
+    {
+        public Rule(ConditionData? condition, string destination)
+        {
+            Condition = condition;
+            Destination = destination;
+        }
+
+        public ConditionData? Condition { get; }
+        public string Destination { get; }
+    }
+
+    public struct PartRange
+    {
+        public PartRange(Range x, Range m, Range a, Range s)
+        {
+            X = x;
+            M = m;
+            A = a;
+            S = s;
+        }
+
+        public Range X { get; }
+        public Range M { get; }
+        public Range A { get; }
+        public Range S { get; }
+    }
+}
