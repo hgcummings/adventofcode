1d9b7bf9ead81fee5b36f0ea9d39667b3a1da041
Harry Cummings <harry@xlr3.com>
2024-12-02 05:25:48 +0000

    2024 day 2

---
+++ HGC.AOC.2024/02/Part1.cs
@@ -0,0 +1,53 @@
+﻿using HGC.AOC.Common;
+
+namespace HGC.AOC._2024._02;
+
+public class Part1 : ISolution
+{
+    public object? Answer()
+    {
+        var input = this.ReadInputLines("input.txt");
+
+        return input.Count(line =>
+        {
+            int? direction = null;
+            int? prev = null;
+            foreach (var entry in line.
+                         Split(' ', StringSplitOptions.RemoveEmptyEntries).
+                         Select(Int32.Parse))
+            {
+                if (prev.HasValue)
+                {
+                    if (direction.HasValue)
+                    {
+                        if (Math.Sign(entry - prev.Value) != direction)
+                        {
+                            Console.WriteLine($"{line} is unsafe: {prev} to {entry} changes direction");
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        direction = Math.Sign(entry - prev.Value);
+                        if (direction == 0)
+                        {
+                            return false;
+                        }
+                    }
+
+                    var distance = Math.Abs(entry - prev.Value);
+                    if (distance is < 1 or > 3)
+                    {
+                        Console.WriteLine($"{line} is unsafe: {prev} to {entry} differ by {distance}");
+                        return false;
+                    }
+                }
+
+                prev = entry;
+            }
+
+            Console.WriteLine($"{line} is safe");
+            return true;
+        });
+    }
+}
---
+++ HGC.AOC.2024/02/Part2.cs
@@ -0,0 +1,64 @@
+﻿using HGC.AOC.Common;
+
+namespace HGC.AOC._2024._02;
+
+public class Part2 : ISolution
+{
+    public object? Answer()
+    {
+        var input = this.ReadInputLines("input.txt");
+
+        return input.Count(line => IsSafe(line.
+            Split(' ', StringSplitOptions.RemoveEmptyEntries).
+            Select(Int32.Parse).ToList())
+        );
+    }
+
+    private bool IsSafe(List<int> report)
+    {
+        return IsSafe(report, null) || 
+               Enumerable.Range(0, report.Count).Any(skip => IsSafe(report, skip));
+    }
+    
+    private bool IsSafe(List<int> report, int? skip) {
+        int? direction = null;
+        int? prev = null;
+        for (var i = 0; i < report.Count; ++i)
+        {
+            if (i == skip)
+            {
+                continue;
+            }
+
+            var entry = report[i];
+            
+            if (prev.HasValue)
+            {
+                if (direction.HasValue)
+                {
+                    if (Math.Sign(entry - prev.Value) != direction)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    direction = Math.Sign(entry - prev.Value);
+                    if (direction == 0)
+                    {
+                        return false;
+                    }
+                }
+
+                var distance = Math.Abs(entry - prev.Value);
+                if (distance is < 1 or > 3)
+                {
+                    return false;
+                }
+            }
+
+            prev = entry;
+        }
+        return true;
+    }
+}
