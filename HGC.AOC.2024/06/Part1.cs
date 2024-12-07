93f2ad401452d2ff997873e90ad6da78e36bf31c
Harry Cummings <harry@xlr3.com>
2024-12-07 07:50:41 +0000

    2024 day 6

---
+++ HGC.AOC.2024/06/Part1.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+using HGC.AOC.Common;
+
+namespace HGC.AOC._2024._06;
+
+public class Part1 : ISolution
+{
+    public object? Answer()
+    {
+        var input = this.ReadInputLines("input.txt").ToList();
+
+        var items = input
+            .SelectMany((line, y) => line.Select((c, x) => (c, new Point(x, y))))
+            .Where(i => i.c != '.')
+            .ToList();
+
+        var obstacles = items.Where(i => i.c == '#').Select(i => i.Item2).ToHashSet();
+        (Point pos, int dir) guard = (items.Single(i => i.c == '^').Item2, 0);
+
+        var visited = new HashSet<Point>();
+
+        bool InBounds(Point pos)
+        {
+            return pos.X >= 0 && pos.Y >= 0 && pos.X < input[0].Length && pos.Y < input.Count;
+        }
+
+        while (InBounds(guard.pos))
+        {
+            visited.Add(guard.pos);
+            var nextPos = guard.dir switch
+            {
+                0 => guard.pos with { Y = guard.pos.Y - 1 },
+                1 => guard.pos with { X = guard.pos.X + 1 },
+                2 => guard.pos with { Y = guard.pos.Y + 1 },
+                3 => guard.pos with { X = guard.pos.X - 1 },
+            };
+            if (obstacles.Contains(nextPos))
+            {
+                guard.dir = (guard.dir + 1) % 4;
+            }
+            else
+            {
+                guard.pos = nextPos;
+            }
+        }
+
+        return visited.Count;
+    }
+}
---
+++ HGC.AOC.2024/06/Part2.cs
@@ -0,0 +1,73 @@
+﻿using System.Drawing;
+using HGC.AOC.Common;
+
+namespace HGC.AOC._2024._06;
+
+public class Part2 : ISolution
+{
+    public object? Answer()
+    {
+        var input = this.ReadInputLines("input.txt").ToList();
+
+        var items = input
+            .SelectMany((line, y) => line.Select((c, x) => (c, new Point(x, y))))
+            .Where(i => i.c != '.')
+            .ToList();
+
+        var obstacles = items.Where(i => i.c == '#').Select(i => i.Item2).ToHashSet();
+        (Point pos, int dir) guard = (items.Single(i => i.c == '^').Item2, 0);
+
+        bool InBounds(Point pos)
+        {
+            return pos.X >= 0 && pos.Y >= 0 && pos.X < input[0].Length && pos.Y < input.Count;
+        }
+
+        bool EntersLoop((Point pos, int dir) current)
+        {
+            var visited = new HashSet<(Point pos, int dir)>();
+            while (InBounds(current.pos))
+            {
+                if (!visited.Add(current))
+                {
+                    return true;
+                }
+                var nextPos = current.dir switch
+                {
+                    0 => current.pos with { Y = current.pos.Y - 1 },
+                    1 => current.pos with { X = current.pos.X + 1 },
+                    2 => current.pos with { Y = current.pos.Y + 1 },
+                    3 => current.pos with { X = current.pos.X - 1 },
+                };
+                if (obstacles.Contains(nextPos))
+                {
+                    current.dir = (current.dir + 1) % 4;
+                }
+                else
+                {
+                    current.pos = nextPos;
+                }
+            }
+
+            return false;
+        }
+
+        var count = 0;
+        for (var y = 0; y < input.Count; ++y)
+        {
+            for (var x = 0; x < input[0].Length; ++x)
+            {
+                var point = new Point(x, y);
+                if (point == guard.pos) continue;
+                if (!obstacles.Add(point)) continue;
+                if (EntersLoop(guard))
+                {
+                    ++count;
+                }
+
+                obstacles.Remove(point);
+            }
+        }
+
+        return count;
+    }
+}
