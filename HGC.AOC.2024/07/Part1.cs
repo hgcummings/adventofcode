73a8ae63d2ceab622fdb67bf9ba92b0e09e27d91
Harry Cummings <harry@xlr3.com>
2024-12-07 09:18:28 +0000

    2024 day 7

---
+++ HGC.AOC.2024/07/Part1.cs
@@ -0,0 +1,37 @@
+﻿using HGC.AOC.Common;
+
+namespace HGC.AOC._2024._07;
+
+public class Part1 : ISolution
+{
+    public object? Answer()
+    {
+        List<(long output, List<long> inputs)> lines =
+            this.ReadInputLines("input.txt").Select(line =>
+            {
+                var parts = line.Split(":");
+                return (Int64.Parse(parts[0]),
+                    parts[1].Trim().Split(' ').Select(Int64.Parse).ToList());
+            }).ToList();
+
+        return lines.Sum(line =>
+        {
+            for (var i = 0; i < 1 << (line.inputs.Count - 1); ++i)
+            {
+                var operators = Enumerable.Range(0, line.inputs.Count - 1)
+                    .Select(j => (1 << j & i) == 0);
+
+                var total = operators.Zip(line.inputs.Skip(1))
+                    .Aggregate(line.inputs[0], (curr, next) =>
+                        next.First ? (curr * next.Second) : (curr + next.Second));
+
+                if (total == line.output)
+                {
+                    return line.output;
+                }
+            }
+            
+            return 0;
+        });
+    }
+}
---
+++ HGC.AOC.2024/07/Part2.cs
@@ -0,0 +1,49 @@
+﻿using HGC.AOC.Common;
+
+namespace HGC.AOC._2024._07;
+
+public class Part2 : ISolution
+{
+    public object? Answer()
+    {
+        List<(long output, List<long> inputs)> lines =
+            this.ReadInputLines("input.txt").Select(line =>
+            {
+                var parts = line.Split(":");
+                return (Int64.Parse(parts[0]),
+                    parts[1].Trim().Split(' ').Select(Int64.Parse).ToList());
+            }).ToList();
+
+        return lines.Where(IsValid).Sum(line => line.output);
+    }
+
+    bool IsValid((long output, List<long> inputs) line)
+    {
+        if (line.inputs.Count == 1)
+        {
+            return line.output == line.inputs[0];
+        }
+        
+        var last = line.inputs[^1];
+        
+        if (line.output % last == 0 && IsValid((line.output / last, line.inputs[..^1])))
+        {
+            return true;
+        }
+
+        if (line.output > last && IsValid((line.output - last, line.inputs[..^1])))
+        {
+            return true;
+        }
+
+        var lastString = last.ToString();
+        var outString = line.output.ToString();
+        if (outString.EndsWith(lastString) && outString.Length > lastString.Length &&
+            IsValid((Int64.Parse(outString[..^lastString.Length]), line.inputs[..^1])))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
