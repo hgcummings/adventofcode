d9608b530843b2e5420a192ee14bd462309305c3
Harry Cummings <harry@xlr3.com>
2024-12-18 09:55:59 +0000

    2024 day 6 tidy-up

--- HGC.AOC.2024/06/Part2.cs
+++ HGC.AOC.2024/06/Part2.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.Collections.Immutable;
+using System.Drawing;
 using HGC.AOC.Common;
 
 namespace HGC.AOC._2024._06;
@@ -17,49 +18,17 @@ public class Part2 : ISolution
         var obstacles = items.Where(i => i.c == '#').Select(i => i.Item2).ToHashSet();
         (Point pos, int dir) guard = (items.Single(i => i.c == '^').Item2, 0);
 
-        bool InBounds(Point pos)
-        {
-            return pos.X >= 0 && pos.Y >= 0 && pos.X < input[0].Length && pos.Y < input.Count;
-        }
-
-        bool EntersLoop((Point pos, int dir) current)
-        {
-            var visited = new HashSet<(Point pos, int dir)>();
-            while (InBounds(current.pos))
-            {
-                if (!visited.Add(current))
-                {
-                    return true;
-                }
-                var nextPos = current.dir switch
-                {
-                    0 => current.pos with { Y = current.pos.Y - 1 },
-                    1 => current.pos with { X = current.pos.X + 1 },
-                    2 => current.pos with { Y = current.pos.Y + 1 },
-                    3 => current.pos with { X = current.pos.X - 1 },
-                };
-                if (obstacles.Contains(nextPos))
-                {
-                    current.dir = (current.dir + 1) % 4;
-                }
-                else
-                {
-                    current.pos = nextPos;
-                }
-            }
-
-            return false;
-        }
-
         var count = 0;
-        for (var y = 0; y < input.Count; ++y)
+        var width = input[0].Length;
+        var height = input.Count;
+        for (var y = 0; y < height; ++y)
         {
-            for (var x = 0; x < input[0].Length; ++x)
+            for (var x = 0; x < width; ++x)
             {
                 var point = new Point(x, y);
                 if (point == guard.pos) continue;
                 if (!obstacles.Add(point)) continue;
-                if (EntersLoop(guard))
+                if (EntersLoop(guard, obstacles, width, height))
                 {
                     ++count;
                 }
@@ -69,5 +38,35 @@ public class Part2 : ISolution
         }
 
         return count;
+    }
+
+    bool EntersLoop((Point pos, int dir) current, ISet<Point> obstacles, int width, int height)
+    {
+        var visited = new HashSet<(Point pos, int dir)>();
+        while (current.pos.X >= 0 && current.pos.Y >= 0 && 
+               current.pos.X < width && current.pos.Y < height)
+        {
+            if (!visited.Add(current))
+            {
+                return true;
+            }
+            var nextPos = current.dir switch
+            {
+                0 => current.pos with { Y = current.pos.Y - 1 },
+                1 => current.pos with { X = current.pos.X + 1 },
+                2 => current.pos with { Y = current.pos.Y + 1 },
+                3 => current.pos with { X = current.pos.X - 1 },
+            };
+            if (obstacles.Contains(nextPos))
+            {
+                current.dir = (current.dir + 1) % 4;
+            }
+            else
+            {
+                current.pos = nextPos;
+            }
+        }
+
+        return false;
     }
 }
