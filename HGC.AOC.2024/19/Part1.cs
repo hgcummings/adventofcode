0055fe0c84a49bace97c63482b9320f6516d52cf
Harry Cummings <harry@xlr3.com>
2024-12-19 05:22:57 +0000

    2024 day 19

---
+++ HGC.AOC.2024/19/Part1.cs
@@ -0,0 +1,55 @@
+﻿using System.Drawing;
+using HGC.AOC.Common;
+
+namespace HGC.AOC._2024._19;
+
+public class Part1 : ISolution
+{
+    public object? Answer()
+    {
+        var input = this.ReadInputLines("input.txt").ToList();
+
+        var towels = input[0].Split(", ").OrderByDescending(t => t.Length).ToList();
+
+        Console.WriteLine(String.Join(',', towels));
+        
+        var patterns = input[2..];
+
+        var cache = new Dictionary<string, bool>();
+        cache[String.Empty] = true;
+        foreach (var towel in towels)
+        {
+            cache[towel] = true;
+        }
+
+        bool IsPossible(string pattern)
+        {
+            if (cache.TryGetValue(pattern, out var possible))
+            {
+                return possible;
+            }
+
+            var result = IsPossibleRaw(pattern);
+            cache[pattern] = result;
+            return result;
+        }
+
+        bool IsPossibleRaw(String pattern) {
+            foreach (var towel in towels)
+            {
+                if (pattern.StartsWith(towel) && IsPossible(pattern[towel.Length..]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        return patterns.Count(p =>
+        {
+            Console.WriteLine(p);
+            return IsPossible(p);
+        });
+    }
+}
---
+++ HGC.AOC.2024/19/Part2.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+using HGC.AOC.Common;
+
+namespace HGC.AOC._2024._19;
+
+public class Part2 : ISolution
+{
+    public object? Answer()
+    {
+        var input = this.ReadInputLines("input.txt").ToList();
+
+        var towels = input[0].Split(", ").OrderByDescending(t => t.Length).ToList();
+        
+        var patterns = input[2..];
+
+        var cache = new Dictionary<string, long>();
+        cache[String.Empty] = 1;
+
+        long Options(string pattern)
+        {
+            if (cache.TryGetValue(pattern, out var options))
+            {
+                return options;
+            }
+
+            var result = OptionsRaw(pattern);
+            cache[pattern] = result;
+            return result;
+        }
+
+        long OptionsRaw(String pattern)
+        {
+            return towels.Sum(t => pattern.StartsWith(t) ? Options(pattern[t.Length..]) : 0);
+        }
+
+        return patterns.Sum(Options);
+    }
+}
