a92fbce27c0e994f026c4e4cfb0c5a7a5c6d312a
Harry Cummings <harry@xlr3.com>
2024-12-19 07:28:16 +0000

    2024 day 19 optimisation

--- HGC.AOC.2024/19/Part2.cs
+++ HGC.AOC.2024/19/Part2.cs
@@ -1,4 +1,4 @@
-﻿using System.Drawing;
+﻿using System.Collections.Concurrent;
 using HGC.AOC.Common;
 
 namespace HGC.AOC._2024._19;
@@ -8,31 +8,15 @@ public class Part2 : ISolution
     public object? Answer()
     {
         var input = this.ReadInputLines("input.txt").ToList();
+        var towels = input[0].Split(", ").ToList();
 
-        var towels = input[0].Split(", ").OrderByDescending(t => t.Length).ToList();
-        
-        var patterns = input[2..];
+        var cache = new ConcurrentDictionary<string, long>();
+        cache.TryAdd(String.Empty, 1);
 
-        var cache = new Dictionary<string, long>();
-        cache[String.Empty] = 1;
+        long Options(string pattern) =>
+            cache.GetOrAdd(pattern,
+                p => towels.Sum(t => p.StartsWith(t) ? Options(p[t.Length..]) : 0));
 
-        long Options(string pattern)
-        {
-            if (cache.TryGetValue(pattern, out var options))
-            {
-                return options;
-            }
-
-            var result = OptionsRaw(pattern);
-            cache[pattern] = result;
-            return result;
-        }
-
-        long OptionsRaw(String pattern)
-        {
-            return towels.Sum(t => pattern.StartsWith(t) ? Options(pattern[t.Length..]) : 0);
-        }
-
-        return patterns.Sum(Options);
+        return input[2..].AsParallel().Sum(Options);
     }
 }
