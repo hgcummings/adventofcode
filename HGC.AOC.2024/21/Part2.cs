61fc2557ffe035c36076bd3c370d28a5f204f5ad
Harry Cummings <harry@xlr3.com>
2024-12-21 13:01:09 +0000

    2024 day 21 tidy-up

--- HGC.AOC.2024/21/Part2.cs
+++ HGC.AOC.2024/21/Part2.cs
@@ -24,23 +24,16 @@ public class Part2 : ISolution
         return this.ReadInputLines("input.txt").Sum(code =>
             ExpandedCost(25, ShortestSequence(Numeric, code)) * Int64.Parse(code[..^1]));
     }
-
-    readonly struct CacheKey(int rounds, string seq)
-    {
-        public int Rounds { get; } = rounds;
-        public String Seq { get; } = seq;
-    }
-
-    private ConcurrentDictionary<CacheKey, long> costCache = new();
     
+    private ConcurrentDictionary<(int rnd, string str), long> costCache = new();
+
     long ExpandedCost(int rounds, string input)
     {
-        var cacheKey = new CacheKey(rounds, input);
-
-        return costCache.GetOrAdd(cacheKey, key => rounds == 1
-            ? ShortestSequence(Directional, key.Seq).Length
-            : key.Seq.Split('A')
-                .Sum(sub => ExpandedCost(key.Rounds - 1, ShortestSequence(Directional, sub + 'A'))) - 1);
+        return costCache.GetOrAdd((rounds, input), key => rounds == 1
+            ? ShortestSequence(Directional, key.str).Length
+            : key.str.Split('A')
+                .Sum(sub =>
+                    ExpandedCost(key.rnd - 1, ShortestSequence(Directional, sub + 'A'))) - 1);
     }
 
     string ShortestSequence(List<string> keypad, string input)
