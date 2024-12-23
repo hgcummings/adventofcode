106ddcb79117c52ec0cf42e92bb75d899052db05
Harry Cummings <harry@xlr3.com>
2024-12-23 06:22:09 +0000

    2024 day 23

---
+++ HGC.AOC.2024/23/Part1.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+using HGC.AOC.Common;
+
+namespace HGC.AOC._2024._23;
+
+public class Part1 : ISolution
+{
+    public object? Answer()
+    {
+        var links = new Dictionary<string, List<string>>();
+
+        void AddLink(string from, string to)
+        {
+            if (!links.ContainsKey(from))
+            {
+                links[from] = new List<string>();
+            }
+            links[from].Add(to);
+        }
+        
+        foreach (var line in this.ReadInputLines("input.txt"))
+        {
+            var parts = line.Split('-');
+            AddLink(parts[0], parts[1]);
+            AddLink(parts[1], parts[0]);
+        }
+
+        var triples = new List<string>();
+
+        foreach (var entry in links)
+        {
+            foreach (var first in entry.Value)
+            {
+                foreach (var second in entry.Value)
+                {
+                    if (first != second && links[first].Contains(second))
+                    {
+                        triples.Add(String.Join(',',
+                            new[] { entry.Key, first, second }.Order()));
+                    } 
+                }
+            }
+        }
+
+        triples = triples.Distinct().ToList();
+
+        return triples.Count(t => t[0] == 't' || t[3] == 't' || t[6] == 't');
+    }
+}
---
+++ HGC.AOC.2024/23/Part2.cs
@@ -0,0 +1,65 @@
+﻿using Combinatorics.Collections;
+using HGC.AOC.Common;
+
+namespace HGC.AOC._2024._23;
+
+public class Part2 : ISolution
+{
+    public object? Answer()
+    {
+        var links = new Dictionary<string, List<string>>();
+
+        void AddLink(string from, string to)
+        {
+            if (!links.ContainsKey(from))
+            {
+                links[from] = new List<string>();
+            }
+            links[from].Add(to);
+        }
+        
+        foreach (var line in this.ReadInputLines("input.txt"))
+        {
+            var parts = line.Split('-');
+            AddLink(parts[0], parts[1]);
+            AddLink(parts[1], parts[0]);
+        }
+
+        for (var n = 4;; ++n)
+        {
+            Console.WriteLine(n);
+            
+            var networks = FindSubsets(links, n)
+                .Select(s => String.Join(',', s.Order())).Distinct().ToList();
+
+            if (networks.Count == 1)
+            {
+                return networks[0];
+            }
+
+            foreach (var network in networks)
+            {
+                Console.WriteLine(network);
+            }
+            
+            Console.WriteLine();
+        }
+    }
+
+    IEnumerable<IEnumerable<string>> FindSubsets(Dictionary<string, List<string>> links, int n)
+    {
+        foreach (var entry in links)
+        {
+            var combinations = new Combinations<string>(entry.Value, n - 1);
+
+            foreach (var combination in combinations)
+            {
+                if (combination.All(n1 => combination.All(n2 => n1 == n2 || links[n1].Contains
+                        (n2))))
+                {
+                    yield return combination.Concat(new[] {entry.Key});
+                }
+            }
+        }
+    }
+}
