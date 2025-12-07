e70ef8e86bd1f63f51a75e1af559074693851934
Harry Cummings <harry@xlr3.com>
2025-12-07 07:21:27 +0000

    2025 day 3

---
+++ HGC.AOC.2025/03/Part1.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using HGC.AOC.Common;
+
+namespace HGC.AOC._2025._03;
+
+public class Part1 : ISolution
+{
+    public object? Answer()
+    {
+        return this.ReadInputLines().Sum(HighestJoltage);
+    }
+
+    public int HighestJoltage(string bank)
+    {
+        Console.Write(bank);
+        var major = bank.Substring(0, bank.Length - 1).Max();
+        var majorIndex = bank.IndexOf(major);
+        var minor = bank.Substring(majorIndex + 1).Max();
+        var total = Int32.Parse(new StringBuilder().Append(major).Append(minor).ToString());
+        Console.WriteLine($" -> {total}");
+        return total;
+    }
+    
+    
+}
---
+++ HGC.AOC.2025/03/Part2.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using HGC.AOC.Common;
+
+namespace HGC.AOC._2025._03;
+
+public class Part2 : ISolution
+{
+    public object? Answer()
+    {
+        return this.ReadInputLines().Sum(HighestJoltage);
+    }
+
+    public long HighestJoltage(string bank)
+    {
+        var currentIndex = 0;
+        var totalBuilder = new StringBuilder();
+        for (var i = 12; i > 0; --i)
+        {
+            var highestOption = bank[currentIndex..(bank.Length - i + 1)].Max();
+            currentIndex = bank.IndexOf(highestOption, currentIndex) + 1;
+            totalBuilder.Append(highestOption);
+        }
+        
+        Console.Write(bank);
+        var total = Int64.Parse(totalBuilder.ToString());
+        Console.WriteLine($" -> {total}");
+        return total;
+    }
+    
+    
+}
