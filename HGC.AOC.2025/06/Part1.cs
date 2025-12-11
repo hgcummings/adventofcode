e9227ab939e6439d99e0e75a6180a4ae7f3c4458
Harry Cummings <harry@xlr3.com>
2025-12-11 08:31:38 +0000

    2025 day 6

---
+++ HGC.AOC.2025/06/Part1.cs
@@ -0,0 +1,47 @@
+﻿using HGC.AOC.Common;
+
+namespace HGC.AOC._2025._06;
+
+public class Part1 : ISolution
+{
+    public object? Answer()
+    {
+        List<List<long>> operands = null;
+        var operators = new List<string>();
+
+        foreach (var line in this.ReadInputLines())
+        {
+            var elements = line
+                .Split(' ',
+                    StringSplitOptions.TrimEntries |
+                    StringSplitOptions.RemoveEmptyEntries);
+            if (line[0] == '*' || line[0] == '+')
+            {
+                operators = elements.ToList();
+                break;
+            }
+
+            if (operands == null)
+            {
+                operands = new List<List<long>>();
+
+                for (var i = 0; i < elements.Length; ++i)
+                {
+                    operands.Add(new List<long>());
+                }
+            }
+            
+            for (var i = 0; i < elements.Length; ++i)
+            {
+                operands[i].Add(Int64.Parse(elements[i]));
+            }
+        }
+
+        return operators.Select((op, i) => op switch
+        {
+            "*" => operands[i].Aggregate(1L, (acc, cur) => acc * cur),
+            "+" => operands[i].Sum(),
+            _ => throw new InvalidOperationException()
+        }).Sum();
+    }
+}
---
+++ HGC.AOC.2025/06/Part2.cs
@@ -0,0 +1,69 @@
+﻿using HGC.AOC.Common;
+
+namespace HGC.AOC._2025._06;
+
+public class Part2 : ISolution
+{
+    public object? Answer()
+    {
+        List<string> rawOperands = new List<string>();
+        string operators = null!;
+
+        foreach (var line in this.ReadInputLines())
+        {
+            if (line[0] == '*' || line[0] == '+')
+            {
+                operators = line;
+                break;
+            }
+            
+            rawOperands.Add(line);
+        }
+
+        var total = 0L;
+
+        for (var i = 0; i < operators.Length; ++i)
+        {
+            if (operators[i] != ' ')
+            {
+                Console.Write(operators[i]);
+                var end = operators.IndexOfAny(['*', '+'], i + 1) - 1;
+                if (end == -2)
+                {
+                    end = operators.Length;
+                }
+                var operands = new List<long>();
+            
+                for (var j = i; j < end; ++j)
+                {
+                    var operand = 0L;
+                    var unit = 1L;
+                    for (var k = rawOperands.Count - 1; k >=0; --k)
+                    {
+                        var digit = rawOperands[k].Substring(j, 1);
+                        if (digit != " ")
+                        {
+                            operand += unit * Int64.Parse(digit);
+                            unit *= 10;
+                        }
+                    }
+                    operands.Add(operand);
+                }
+                
+                Console.Write(String.Join(' ', operands));
+                
+                var result = operators[i] switch
+                {
+                    '*' => operands.Aggregate(1L, (acc, cur) => acc * cur),
+                    '+' => operands.Sum(),
+                    _ => throw new InvalidOperationException()
+                };
+                
+                Console.WriteLine($" = {result}");
+                total += result;
+            }
+        }
+
+        return total;
+    }
+}
